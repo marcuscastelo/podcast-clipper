ad7bfb4c06c350ec81796904c5907974dd65e68a
marcuscastelo <marcuscastelo@usp.br>
2021-06-04 03:46:21 -0300

    Hours of insanity didn't find error filter_complex

--- src/ffmpeg-wrapper.ts
+++ src/ffmpeg-wrapper.ts
@@ -1,55 +1,83 @@
+import { strict } from 'assert/strict';
 import chalk from 'chalk';
 import ffmpeg from 'ffmpeg'
 import Process from './process'
+import { Optional } from 'utility-types'
 
-const videoFolder = './videos'
-
-
-type Clip = {
-    filename: string,
-    start: number,
-    end: number,
+class Clip {
+    constructor(
+        public original_filename: string,
+        public start: number,
+        public end: number,
+    ) { }
 }
+
+type Range = {start: number, end: number};
+
+const mapRange = ({start, end}: Optional<Range, 'start'>) => 
+                    (func: (i: number) => string) =>
+                        new Array(end - (start ?? 0)).fill(0)
+                            .map((_, index) => func(index + (start ?? 0)))
+                            .reduce((a, b) => `${a} ${b}`);
+
+
+const mapInputs = (inputs: string[]) => inputs.map(inp => ['-i', inp]).reduce((a,b) => [...a, ...b]);
+
+const va = (i: number) =>`[${i}:v] [${i}:a]`;
+
+const createConcatFilter = 
+    (inputCount: number) => 
+        `${mapRange({end: inputCount})(va)} concat=n=${inputCount}:v=1:a=1 [v] [a]`
 
 //Limited to one output for now
-export default class FFMpeg {
-    
+export default class Concatenation {
+
+    private inputFileNames: string[] = [];
+
+    constructor(public readonly outputFileName: string) {}
+
+    public addInputFiles(...inputFileNames: string[]) {
+        this.inputFileNames.push(...inputFileNames);
+    }
+
+    public async create() {
+        const globalOptions = [
+            '-y' //Say yes to all (e.g. overwriting)
+        ]
+
+        const filters = [
+            '-filter_complex',
+            `"${createConcatFilter(this.inputFileNames.length)}"`
+        ]
+
+        const maps = [
+            '-map',
+            '"[v]"',
+            '-map',
+            '"[a]"'
+        ]
+
+        const args = [
+            ...globalOptions,
+            ...mapInputs(this.inputFileNames),
+            ...filters,
+            ...maps,
+            this.outputFileName
+        ];
+        
+        // //TODO: Windows and Mac
+        const command = 'ffmpeg';
+
+        // console.log(args)
+
+        let proc = new Process(command, args);
+        console.log(proc.getCompleteCMD());
+
+        proc.stdoutPrintFunction = (m: string) => console.log(chalk.reset(m));
+        proc.stderrPrintFunction = (m: string) => console.error(chalk.red(m));
+
+        proc.spawn();
+        await proc.join();
+
+    }
 };
-
-export async function testSpawnFFMPeg() {
-
-    const globalOptions = [
-        '-y' //Say yes to all (e.g. overwriting)
-    ]
-
-    const inputFiles = [
-        '-i',
-        `${videoFolder}/intro.mp4`,
-        '-i',
-        `${videoFolder}/videotest.mp4`,
-        '-i',
-        `${videoFolder}/intro.mp4`,
-    ]
-
-    const filters = [
-        '-filter_complex',
-        '"[0:v] [0:a] [1:v] [1:a] [2:v] [2:a] concat=n=3:v=1:a=1 [v] [a]"'
-    ]
-
-    const maps = [
-        '-map',
-        '"[v]"',
-        '-map',
-        '"[a]"'
-    ]
-
-    let proc = new Process(`ffmpeg`, [...globalOptions, ...inputFiles, ...filters, ...maps, `${videoFolder}/out.mkv`]);
-    console.log(proc.getCompleteCMD());
-
-    proc.stdoutPrintFunction = (m: string) => console.log(chalk.reset(m));
-    proc.stderrPrintFunction = (m: string) => console.error(chalk.red(m));
-  
-    proc.spawn();
-    await proc.join();
-
-}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import chalk from 'chalk'
 import Process from './process'
-import FFMpeg, {testSpawnFFMPeg} from './ffmpeg-wrapper'
+import Concatenation from './ffmpeg-wrapper'
 
 const intro = './videos/intro.mp4'
 const videotest = './videos/videotest.mp4'
@@ -11,13 +11,16 @@ const out = './videos/out/out.mp4'
 async function main() {
   console.log(`\n\n Iniciando ${chalk.yellow('Podcast clipper')}....\n\n`)
 
-  await testSpawnFFMPeg();
+  const concat = new Concatenation('./videos/out.mkv');
+  concat.addInputFiles(  
+    `./videos/intro.mp4`,
+    `./videos/intro.mp4`,
+    `./videos/intro.mp4`,
+  );
 
-  // let proc = new Process('echo', ['oi', '"a b c d e "']);
-  // proc.stdoutPrintFunction = console.log;
-  // proc.spawn();
-  // await proc.join();
+  concat.create();
 
+  
   console.log('\n\n Podcast clipper encerrando... Volte sempre! n\n')
 }
 
