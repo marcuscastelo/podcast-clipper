106b8d7de8f6aefa7075d9bd7bf87526f03cd658
marcuscastelo <marcuscastelo@usp.br>
2021-06-04 01:05:42 -0300

    Testing child_process, starting wrappers

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-let text: string = "Hello World!";
+import fs from 'fs'
+import chalk from 'chalk'
+import Process from './process'
 
-function typedFunction(parameter: string): void {
-    console.log(parameter);
+const intro = './videos/intro.mp4'
+const videotest = './videos/videotest.mp4'
+
+const out = './videos/out/out.mp4'
+
+async function main() {
+  console.log(`\n\n Iniciando ${chalk.yellow('Podcast clipper')}....\n\n`)
+
+  let proc = new Process('watch', ['-n', '1', 'date']);
+
+  proc.stdinPrintFunction = console.log;
+  proc.stderrPrintFunction = console.error;
+
+  proc.spawn();
+  await proc.join();
+
+  console.log('Programa encerrado, o stdin completo foi esse: ', proc.getOutput())
+
+  console.log('\n\n Podcast clipper encerrando... Volte sempre! n\n')
 }
 
-typedFunction(text);
+main();
