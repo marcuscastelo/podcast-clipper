3c6f711176d414fcc891d3c212212c722c779fba
marcuscastelo <marcuscastelo@usp.br>
2022-07-11 13:53:53 -0300

    Basic test CLI

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,33 @@
 import chalk from 'chalk'
 import { testSpotify } from './spotify-test';
 
+import { program } from 'commander';
+program.option('-s, --spotify', 'Test Spotify API');
+program.option('-y, --youtube', 'Test YouTube API');
+program.option('-d, --download', 'Download YouTube videos');
+
+program.parse();
+
 async function main() {
-    await testSpotify();
+    const opts = program.opts();
+    // Assert only one option is set
+    if (Object.keys(opts).length !== 1) {
+        console.log(chalk.red('Exactly one option must be set'));
+        process.exit(1);
+    }
+
+    if (opts.spotify) {
+        await testSpotify();
+    } else if (opts.youtube) {
+        console.log(chalk.red('Not implemented'));
+        throw new Error('Not implemented - Vai la vinão');
+    } else if (opts.download) {
+        console.log(chalk.red('Not implemented'));
+        throw new Error('Not implemented');
+    } else {
+        console.log(chalk.red('No option set'));
+        process.exit(1);
+    }
 }
 
 main();
