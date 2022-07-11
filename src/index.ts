6c718d6d102c12f8a19c84808c9575996b37e822
marcuscastelo <marcuscastelo@usp.br>
2022-07-10 22:16:10 -0300

    Remove old code

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,6 @@
-import fs from 'fs'
 import chalk from 'chalk'
-import Process from './process'
-import Concatenation from './ffmpeg-wrapper'
-
-const intro = './videos/intro.mp4'
-const videotest = './videos/videotest.mp4'
-
-const out = './videos/out/out.mp4'
 
 async function main() {
-  console.log(`\n\n Iniciando ${chalk.yellow('Podcast clipper')}....\n\n`)
-
-  const concat = new Concatenation('./videos/out.mkv');
-  concat.addInputFiles(  
-    `./videos/intro.mp4`,
-    `./videos/intro.mp4`,
-    `./videos/intro.mp4`,
-  );
-
-  concat.create();
-
-  
-  console.log('\n\n Podcast clipper encerrando... Volte sempre! n\n')
 }
 
 main();
